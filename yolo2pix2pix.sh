891710892f4034cac84051786cd5654532b10faf
Noobtoss <thomasschmittba17196@yahoo.de>
2024-05-15 15:45:59 +0200

    -update

--- yolo2pix2pix.sh
+++ yolo2pix2pix.sh
@@ -3,4 +3,4 @@
 eval "$(conda shell.bash hook)"
 conda activate CycleGAN
 
-python datasets/combine_A_and_B.py --fold_A datasets/cycleGAN/example/masks --fold_B datasets/cycleGAN/example/images --fold_AB datasets/pix2pix/example --no_multiprocessing
+python datasets/combine_A_and_B.py --fold_A datasets/input/example/images --fold_B datasets/input/example/masks --fold_AB datasets/pix2pix/example --no_multiprocessing
